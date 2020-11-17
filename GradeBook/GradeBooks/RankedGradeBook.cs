b19cd1e263b4ebec7f6189dce04af4ce1bf7f7dd
Kent <Kentethalion@ARTEMIS>
2020-11-17 18:57:56 -0500

    return ranked letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,62 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var letterGrade = 'F';
+            var grade = 0;
+            var numStudents = Convert.ToDouble(Students.Count);
+
+            if (numStudents < 5)
+            {
+                throw new System.InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var stepCount = Convert.ToInt32(Math.Floor(numStudents * .2));
+            var gradeList = new List<double>();
+
+            foreach(var student in Students)
+            {
+                gradeList.Add(student.AverageGrade);
+            }
+
+            gradeList.Sort((a, b) => b.CompareTo(a));
+
+            for(var i = stepCount; i < gradeList.Count; i += stepCount)
+            {
+                if(averageGrade > gradeList[i])
+                {
+                    grade = i;
+                    break;
+                }
+            }
+
+            switch (grade/stepCount)
+            {
+                case 1:
+                    letterGrade = 'A';
+                    break;
+                case 2:
+                    letterGrade = 'B';
+                    break;
+                case 3:
+                    letterGrade = 'C';
+                    break;
+                case 4:
+                    letterGrade = 'D';
+                    break;
+            }
+
+            return letterGrade;
         }
     }
 }
